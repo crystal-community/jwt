359db77733d4e109fa955ecd05dd7cf09ce8081c
Potapov Sergey <blake131313@gmail.com>
2016-01-24 21:24:36 +0100

    Refactor: extract exceptions into jwt/errors.cr

--- examples/iss_claim.cr
+++ examples/iss_claim.cr
@@ -8,4 +8,3 @@ payload, header = JWT.decode(token, "SecretKey", "HS256", {iss: "me"})
 
 # iss does not match, raises JWT::InvalidIssuerError
 payload, header = JWT.decode(token, "SecretKey", "HS256", {iss: "you"})
-
---
+++ src/jwt/errors.cr
@@ -0,0 +1,25 @@
+module JWT
+  # Basic JWT exception.
+  class Error < ::Exception; end;
+
+  # Is raised on attempt to use unsupported algorithm.
+  class UnsupportedAlogrithmError < Error; end;
+
+  # raised when failed to decode token
+  class DecodeError < Error; end;
+
+  # Is raised when failed to verify signature.
+  class VerificationError < DecodeError; end;
+
+  # Is raised when signature is expired (see `exp` reserved claim name)
+  class ExpiredSignatureError < DecodeError; end;
+
+  # Is raised when time hasn't reached nbf claim in the token.
+  class ImmatureSignatureError < DecodeError; end;
+
+  # Is raised when 'aud' does not match.
+  class InvalidAudienceError < DecodeError; end;
+
+  # Is raised when 'iss' does not match.
+  class InvalidIssuerError < DecodeError; end;
+end
